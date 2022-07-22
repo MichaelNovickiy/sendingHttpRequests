8f07f8d87003a8567538afc48efa3b192073da67
Mike <slavik433@gmail.com>
2022-07-22 14:10:15 +0300

    Updates

---
+++ precache-manifest.99f38a7f0bd331a509dd4d123fcdb613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e89f437c38f1c7fd28220f4f6e9fa1b",
+    "url": "/sendingHttpRequests/index.html"
+  },
+  {
+    "revision": "2cad58f57863c48cab25",
+    "url": "/sendingHttpRequests/static/css/main.cf664a6f.chunk.css"
+  },
+  {
+    "revision": "22daeca5b02b55968147",
+    "url": "/sendingHttpRequests/static/js/2.42d1fe8e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/sendingHttpRequests/static/js/2.42d1fe8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cad58f57863c48cab25",
+    "url": "/sendingHttpRequests/static/js/main.45e56be2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/sendingHttpRequests/static/js/main.45e56be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8151bb8e93850d0e09",
+    "url": "/sendingHttpRequests/static/js/runtime-main.e9e6ed55.js"
+  }
+]);
